d4f9ccb725574e85a1750c71f9ca0f4fc0e73d55
Gina Mirando <gina.mirando5@gmail.com>
2022-08-23 12:23:12 -0500

    fixed props, dropdown for lines and stops

--- src/components/Stops.js
+++ src/components/Stops.js
@@ -1,11 +1,12 @@
 import React from "react";
 import axios from "axios";
+// import dropdown from "Dropdown"
 
 // import Arrivals from "./Arrivals";
 
 export default class StopList extends React.Component {
   state = {
-    line: [],
+    lines: [],
     stops: [],
   };
 
@@ -13,44 +14,46 @@ export default class StopList extends React.Component {
     axios.get(`http://localhost:3000/stops`).then((res) => {
       const stops = res.data;
       this.setState({ stops });
+      console.log(stops);
+      axios.get(`http://localhost:3000/lines`).then((res) => {
+        const lines = res.data;
+        this.setState({ lines });
+        console.log(lines);
+      });
     });
   }
-
-    render() {
-      return (
-        <div>
-          <select>
-            {this.state.stops.map((stop, index) => (
-              <option value={stop.parent_id}>{stop.name}</option>
-            ))}
-          </select>
-        </div>
-      );
-    }
+  
+  render() {
+    return (
+      <div>
+        <Dropdown
+          label="CTA Train Line"
+          options={this.state.lines.map((line, index) => (
+            <option value={line.id}>{line.color}</option>
+          ))}
+        />
+        <Dropdown
+          label="Train Stop"
+          options={this.state.stops.map((stop, index) => (
+            <option value={stop.parent_id}>{stop.name}</option>
+          ))}
+        />
+      </div>
+    );
   }
+}
 
-//   render() {
-//     return (
-//       <div>
-//         <Dropdown
-//           label="CTA Train Line"
-//           options={[
-//             { label: "Blue" },
-//             { label: "Brown" },
-//             { label: "Red" },
-//             { label: "Green" },
-//             { label: "Orange" },
-//             { label: "Purple" },
-//             { label: "Pink" },
-//             { label: "Yellow" },
-//           ]}
-//           value={line}
-//           onChange={pickTrainLine}
-//         />
-//         <Dropdown 
-//         label="Train Stop"
-//         options={this.state.stops.map((stop, index) => <option value={stop.parent_id}>{stop.name}</option>)} />
-//       </div>
-//     );
-//   }
-// }
+const Dropdown = ({ label, value, options, onChange }) => {
+  return (
+    <label>
+      {label}
+      <select value={value} onChange={onChange}>
+        {options.map((option) => (
+          <option key={Math.random()} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+};
